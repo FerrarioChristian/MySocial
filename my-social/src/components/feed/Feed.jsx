8dc82d2ef1143f4afb68626ad6b66154047f249d
Ferra <chri@ferraa.it>
2021-11-21 23:58:07 +0100

    proxy updated

--- my-social/src/components/feed/Feed.jsx
+++ my-social/src/components/feed/Feed.jsx
@@ -1,10 +1,20 @@
 import Post from "components/post/Post";
 import Share from "components/share/Share";
-import { Posts } from "dummyData";
 import React from "react";
+import { useState, useEffect } from "react";
 import "./feed.css";
+import axios from "axios";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = () => {
+      const res = axios.get("posts/timeline/6188614c53e76899d7fd8779");
+      console.log(res);
+    };
+  }, []);
+
   return (
     <div className="feed">
       <div className="feedWrapper">
